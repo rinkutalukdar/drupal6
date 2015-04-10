57592779b16671b7ef1ea25f94fde2c8b5bf83d0
unknown <rtalukda@WJNJINPG0053UC.wks.jnj.com>
2015-04-10 18:04:55 +0530

    Security Update 1

--- drupal-6.35/security_update.sh
+++ drupal-6.35/security_update.sh
@@ -6,7 +6,7 @@
 
 echo "Script starts..."
 
-echo "Git operation for base site............."$'\r' > "base-SU-1.log" 2>&1
+echo "Git operation for base site............."$'\r' >> "base-SU-1.log" 2>&1
 echo -n "Enter git repo-name for site: "
 read repo_name
 GIT checkout production >> "base-SU-1.log" 2>&1
@@ -17,27 +17,23 @@ else
   GIT checkout 'SU-1'
   echo "Already SU-1 that Branch exist"
 fi
-echo $'\r'"Git operation ends for base site............."$'\r' >> "base-SU-1.log" 2>&1
+echo "Git operation ends for base site............."$'\r' >> "base-SU-1.log" 2>&1
 
-echo $'\r'"Security Update for base site........"$'\r' >> "base-SU-1.log" 2>&1
+echo "Security Update for base site........"$'\r' >> "base-SU-1.log" 2>&1
 #drush pm-update --security-only y >> "base-SU-1.log" 2>&1
 drush pm-update -y >> "base-SU-1.log" 2>&1
 drush updatedb >> "base-SU-1.log" 2>&1
-echo $'\r'"Security Update ends for base site..........."$'\r' >> "base-SU-1.log" 2>&1
+echo "Security Update ends for base site..........."$'\r' >> "base-SU-1.log" 2>&1
 
 
 
-echo $'\r'"Git commit and push starts Here........."$'\r' >> "base-SU-1.log" 2>&1
-cd ..
-GIT add .
-cd sites
+echo "Git commit and push starts Here........."$'\r' >> "base-SU-1.log" 2>&1
 GIT commit -m "Security Update 1"
 GIT push $repo_name 'SU-1'
 GIT checkout development
-GIT merge 'SU-1'
-GIT commit -m "Security Update 1"
-GIT push $repo_name development
-echo $'\r'"Git commit and push ends Here........."$'\r' >> "base-SU-1.log" 2>&1
+GIT merge 'SU-1' $'\r' >> "base-SU-1.log" 2>&1
+GIT push $repo_name development $'\r' >> "base-SU-1.log" 2>&1
+echo "Git commit and push starts Here........."$'\r' >> "base-SU-1.log" 2>&1
 
 
 
@@ -59,25 +55,20 @@ do
   echo $site
   if [ $choice -eq 1 ] ; then
     #drush updatedb
-    echo $'\r'"Security Update for $site........"$'\r' >> $site"-SU-1.log" 2>&1
+    echo "Security Update for $site........"$'\r' >> $site"-SU-1.log" 2>&1
 	drush pm-update -y >> $site"-SU-1.log" 2>&1
     #drush pm-update --security-only >> $site"-SU-1.log" 2>&1
     drush updatedb >> $site"-DBUpdate-1.log" 2>&1
-    echo $'\r'"Security Update for $site is completed........"$'\r' >> $site"-SU-1.log" 2>&1
+    echo "Security Update for $site is completed........"$'\r' >> $site"-SU-1.log" 2>&1
   elif [ $choice -eq 4 ] ; then
     drush cc all >> $site"-SU-1.log" 2>&1
   fi
   cd ../
 done
 
-echo $'\r'"Git commit and push starts Here........."$'\r' >> "base-SU-1.log" 2>&1
-cd ..
-GIT add .
-cd sites
-GIT commit -m "Security Update 1"
+echo "Git commit and push starts Here........."$'\r' >> "base-SU-1.log" 2>&1
+GIT cmmit -m "Security Update 1"
 GIT push $repo_name 'SU-1'
 GIT checkout development
-GIT merge 'SU-1'
-GIT commit -m "Security Update 1"
-GIT push $repo_name development
-echo $'\r'"Git commit and push ends Here........."$'\r' >> "base-SU-1.log" 2>&1
+GIT merge 'SU-1' $'\r' >> "base-SU-1.log" 2>&1
+echo "Git commit and push starts Here........."$'\r' >> "base-SU-1.log" 2>&1
